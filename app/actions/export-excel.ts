f1cf9286b6597f8c102e98672e55458743a0a6fc
Manuel <caballeromanuel203@gmail.com>
2025-05-25 20:44:41 +0800

    added export functionality

---
+++ app/actions/export-excel.ts
@@ -0,0 +1,78 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { Workbook } from "exceljs";
+
+export const exportExcelAction = async () => {
+  try {
+    const workBook = new Workbook();
+    const workSheet = workBook.addWorksheet("Sheet 1");
+
+    workSheet.getCell("A1").value = "VOTER ID";
+    workSheet.getCell("B1").value = "NAME";
+    workSheet.getCell("C1").value = "PRECINCT";
+
+    workSheet.getColumn(1).width = 25;
+    workSheet.getColumn(2).width = 50;
+    workSheet.getColumn(3).width = 25;
+
+    [`A1`, `B1`, `C1`].forEach((cellRef) => {
+      const cell = workSheet.getCell(cellRef);
+
+      cell.alignment = {
+        horizontal: "center",
+        vertical: "middle",
+      };
+
+      cell.font = {
+        bold: true,
+        size: 14,
+      };
+    });
+
+    const voters = await prisma.voter.findMany();
+
+    if (voters.length < 1) {
+      throw new Error("Could not export excel file. Data list is empty.");
+    }
+
+    for (let i = 0; i < voters.length; i++) {
+      const column = i + 2;
+
+      workSheet.getCell(`A${column}`).value = voters[i].voterId;
+      workSheet.getCell(`B${column}`).value = voters[i].name;
+      workSheet.getCell(`C${column}`).value = voters[i].precinct;
+
+      [`A${column}`, `B${column}`, `C${column}`].forEach((cellRef) => {
+        const cell = workSheet.getCell(cellRef);
+
+        cell.alignment = {
+          horizontal: "left",
+          vertical: "middle",
+        };
+
+        cell.font = {
+          bold: true,
+          size: 12,
+        };
+
+        if (voters[i].isGiven) {
+          cell.fill = {
+            type: "pattern",
+            pattern: "solid",
+            fgColor: { argb: "9E005DFF" },
+          };
+        }
+      });
+    }
+
+    const buffer = await workBook.xlsx.writeBuffer();
+    const blob = await new Blob([buffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+
+    return { blob, success: true, status: 200 };
+  } catch (error) {
+    return { msg: error, success: false, status: 500 };
+  }
+};
--- app/components/export-button.tsx
+++ app/components/export-button.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { saveAs } from "file-saver";
+import { exportExcelAction } from "../actions/export-excel";
 
 function ExportButton() {
-  return <Button>Export</Button>;
+  const handleExport = async () => {
+    const res = await exportExcelAction();
+
+    if (res.success && res.blob) {
+      saveAs(res.blob as Blob, "voters.xlsx");
+      console.log(res.blob);
+    }
+
+    if (res.success == false) {
+      console.log(res);
+    }
+  };
+
+  return <Button onClick={handleExport}>Export</Button>;
 }
 
 export default ExportButton;
